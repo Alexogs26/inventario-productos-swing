66b8cf6982a8ba6103eaeeac8c27c32ea5078846
Alex <oswaldoalexisgomezsanchez@gmail.com>
2025-12-24 16:15:29 -0600

    Create panel view

--- src/main/java/ag/inventario_swing/gui/InventoryForm.java
+++ src/main/java/ag/inventario_swing/gui/InventoryForm.java
@@ -1,16 +1,24 @@
 package ag.inventario_swing.gui;
 
+import ag.inventario_swing.model.Product;
 import ag.inventario_swing.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.util.List;
 
 @Component
 public class InventoryForm extends JFrame{
     private JPanel mainPanel;
     private JTable productsTable;
+    private JTextField textSku;
+    private JTextField textName;
+    private JTextField textPrice;
+    private JButton guardarButton;
+    private JButton eliminarButton;
+    private JButton limpiarButton;
     private final ProductService productService;
     private DefaultTableModel modelTableProducts;
 
@@ -23,7 +31,7 @@ public class InventoryForm extends JFrame{
     private void initializeForm() {
         setContentPane(mainPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(900, 700);
+        setSize(1000, 800);
         setLocationRelativeTo(null);
     }
 
@@ -32,5 +40,21 @@ public class InventoryForm extends JFrame{
          String[] tableHeaders = {"Id", "Sku", "Nombre", "Precio"};
          modelTableProducts.setColumnIdentifiers(tableHeaders);
          productsTable = new JTable(modelTableProducts);
+
+         listProducts();
+    }
+
+    private void listProducts() {
+        modelTableProducts.setRowCount(0);
+        List<Product> products = productService.listProducts();
+        products.forEach(product -> {
+            Object[] rowProduct = {
+                    product.getId(),
+                    product.getSku(),
+                    product.getName(),
+                    product.getPrice()
+            };
+            modelTableProducts.addRow(rowProduct);
+        });
     }
 }
